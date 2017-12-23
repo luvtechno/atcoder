422f6104d7ad1a73c46ebd3690833a282451feab
Yoshinori Kawasaki <ykawasaki@gmail.com>
2017-12-23 22:20:29 +0900

    Fix TLE

--- abc083/d.rb
+++ abc083/d.rb
@@ -1,3 +1,4 @@
+require 'pp'
 s = gets.chomp.chars
 s = s.map { |e| e == '1' }
 
@@ -7,14 +8,17 @@ ret = len
 pos = []
 z = s[0]
 
+# pp s
 left_len = 0
 loop do
   break if left_len == len
   if s[left_len] != z
-    ret = [left_len, len - left_len].max
-    (left_len..(len-1)).each do |j|
-      s[j] = !s[j]
-    end
+    ret = [ret, [left_len, len - left_len].max].min
+    z = !z
+    # (left_len..(len-1)).each do |j|
+    #   s[j] = !s[j]
+    # end
+    # pp s
   else
     left_len += 1
   end
