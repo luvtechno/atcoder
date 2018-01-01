fd050f054ef4b6b2a45ffadf80410e1a40250f3e
Yoshinori Kawasaki <ykawasaki@gmail.com>
2018-01-02 03:04:12 +0900

    Optimize

--- abc084/d.rb
+++ abc084/d.rb
@@ -1,11 +1,12 @@
 require 'prime'
-instance = Prime.instance
 
 q = gets.chomp.to_i
 
 ls = []
 rs = []
 a = Array.new(10 ** 5, nil)
+b = Array.new(10 ** 5, nil)
+c = Array.new(10 ** 5, 0)
 max = 0
 
 q.times do
@@ -17,12 +18,17 @@ end
 
 Prime.each(max).each { |prime| a[prime] = true }
 
+(1..max).each do |i|
+  b[i] = a[i] && a[(i+1)/2]
+end
+
+(1..max).each do |i|
+  c[i] = c[i-1]
+  c[i] += 1 if b[i]
+end
+
 q.times do |i|
   l = ls[i]
   r = rs[i]
-  count = 0
-  (l..r).step(2) do |n|
-    count += 1 if a[n] && a[(n+1)/2]
-  end
-  puts count
+  puts c[r] - c[l-1]
 end
