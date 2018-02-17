492297cd9af4cd02df116fa5fb111d9d5ed3444f
Yoshinori Kawasaki <ykawasaki@gmail.com>
2018-02-17 16:16:17 +0900

    Optmize

--- future-contest-2018-qual/a.rb
+++ future-contest-2018-qual/a.rb
@@ -6,21 +6,25 @@ STEPS = (ARGV[1] || 450).to_i
 
 N = 100
 
-class F < Struct.new(:mat, :sum)
+class F < Struct.new(:mat, :score)
   def initialize(mat)
     s = 0
-    mat.each { |row| row.each { |e| s += e } }
+    mat.each { |row| row.each { |e| s += e.abs } }
     super(mat, s)
   end
 
   def sub(x, y, h)
+    original_score = self.score
     ([x-h+1, 0].max..[x+h-1, N-1].min).each do |i|
       ([y-h+1, 0].max..[y+h-1, N-1].min).each do |j|
         d = [h - (x - i).abs - (y - j).abs, 0].max
-        self.mat[j][i] -= d
-        self.sum -= d
+        original = self.mat[j][i]
+        self.mat[j][i] = original - d
+        score_diff = original.abs - self.mat[j][i].abs
+        self.score -= score_diff
       end
     end
+    original_score - self.score
   end
 
   def dup
@@ -35,13 +39,23 @@ class F < Struct.new(:mat, :sum)
 end
 
 class Seq < Struct.new(:arr, :target)
+  def initialize(target)
+    super([], target.dup)
+  end
+
+  def dup
+    seq = Seq.new(target)
+    seq.arr = arr.dup
+    seq
+  end
+
   def add!(x, y, h)
     arr << [x, y, h]
     target.sub(x, y, h)
   end
 
   def score
-    target.sum.abs
+    target.score
   end
 
   def gen_rand!(steps = 1000)
@@ -56,15 +70,6 @@ class Seq < Struct.new(:arr, :target)
   end
 
   def gen_rand2!(steps = 1000)
-    250.times do
-      break if (elapsed = Time.now - START_TIME) > TIME_LIMIT
-
-      x = rand(N)
-      y = rand(N)
-      h = N
-      add!(x, y, h)
-    end
-
     (steps - 250).times do
       break if (elapsed = Time.now - START_TIME) > TIME_LIMIT
 
@@ -87,17 +92,24 @@ def solve(target)
   best_score = 200000000
   best_seq = nil
 
+  base_seq = Seq.new(target)
+  250.times do
+    x = rand(N); y = rand(N); h = N
+    base_seq.add!(x, y, h)
+  end
+
   loop do
     break if (elapsed = Time.now - START_TIME) > TIME_LIMIT
-    STDERR.puts "t:#{elapsed} score:#{best_score}"
 
-    seq = Seq.new([], target)
+    seq = base_seq.dup
     seq.gen_rand2!(STEPS)
 
     if seq.score < best_score
       best_score = seq.score
       best_seq = seq
     end
+
+    STDERR.puts "t:#{elapsed} best:#{best_score} score:#{seq.score}"
   end
 
   best_seq
