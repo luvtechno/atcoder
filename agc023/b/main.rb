30f468b5d1c27e6142dbfe35c390c3c744a7cbd8
Yoshinori Kawasaki <ykawasaki@gmail.com>
2018-04-28 21:36:55 +0900

    Solve with go

--- agc023/b/main.rb
+++ agc023/b/main.rb
@@ -1,2 +1,24 @@
 require 'pp'
-N, C = gets.chomp.split(" ").map(&:to_i)
+N = gets.chomp.to_i
+m = []
+N.times do
+  m << gets.chomp.chars
+end
+
+def good(m, a, b)
+  N.times do |i|
+    N.times do |j|
+      return false if m[(i+a)%N][(j+b)%N] != m[(j+a)%N][(i+b)%N]
+    end
+  end
+  true
+end
+
+ans = 0
+N.times do |a|
+  N.times do |b|
+    ans += 1 if good(m, a, b)
+  end
+end
+
+puts ans
