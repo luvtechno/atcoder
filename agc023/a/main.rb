65b3323912699eff75079a6c072b9c17e424a6c9
Yoshinori Kawasaki <ykawasaki@gmail.com>
2018-04-28 21:04:13 +0900

    Solve A

--- agc023/a/main.rb
+++ agc023/a/main.rb
@@ -1,2 +1,15 @@
 require 'pp'
-N, C = gets.chomp.split(" ").map(&:to_i)
+N = gets.chomp.to_i
+a = gets.chomp.split(" ").map(&:to_i)
+
+ans = 0
+
+N.times do |i|
+  sum = 0
+  (i..N-1).each do |j|
+    sum += a[j]
+    ans += 1 if sum == 0
+  end
+end
+
+puts ans
