2ecba797042a46fb82913ac7106b30feb517d8c1
Yoshinori Kawasaki <ykawasaki@gmail.com>
2018-04-28 22:03:53 +0900

    Solve

--- agc023/a/main.rb
+++ agc023/a/main.rb
@@ -3,13 +3,17 @@ N = gets.chomp.to_i
 a = gets.chomp.split(" ").map(&:to_i)
 
 ans = 0
-
+hash = Hash.new(0)
 N.times do |i|
-  sum = 0
-  (i..N-1).each do |j|
-    sum += a[j]
-    ans += 1 if sum == 0
+  next_hash = Hash.new(0)
+  x = a[i]
+  hash.each do |k, v|
+    next_hash[k + x] = v
   end
+  next_hash[x] += 1
+  ans += next_hash[0]
+  hash = next_hash
+  # pp hash
 end
 
 puts ans
