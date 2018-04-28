791906398180f0393b753fd2a0c20800c1c6ece3
Yoshinori Kawasaki <ykawasaki@gmail.com>
2018-04-28 22:13:04 +0900

    Solve in n * log n

--- agc023/a/main.rb
+++ agc023/a/main.rb
@@ -4,16 +4,13 @@ a = gets.chomp.split(" ").map(&:to_i)
 
 ans = 0
 hash = Hash.new(0)
+sum = 0
 N.times do |i|
-  next_hash = Hash.new(0)
   x = a[i]
-  hash.each do |k, v|
-    next_hash[k + x] = v
-  end
-  next_hash[x] += 1
-  ans += next_hash[0]
-  hash = next_hash
-  # pp hash
+  sum += x
+  hash[x - sum] += 1
+  ans += hash[-sum]
+  # pp sum, hash
 end
 
 puts ans
