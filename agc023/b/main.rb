a1addcefd1d077c9fad64bc6fb2c525f8a6686f4
Yoshinori Kawasaki <ykawasaki@gmail.com>
2018-04-28 22:29:13 +0900

    Solve in n ** 3

--- agc023/b/main.rb
+++ agc023/b/main.rb
@@ -1,24 +1,39 @@
 require 'pp'
 N = gets.chomp.to_i
 m = []
+good_m = []
 N.times do
   m << gets.chomp.chars
+  good_m << Array.new(N)
 end
 
-def good(m, a, b)
-  N.times do |i|
-    N.times do |j|
-      return false if m[(i+a)%N][(j+b)%N] != m[(j+a)%N][(i+b)%N]
+
+N.times do |i|
+  N.times do |j|
+    v = 0
+    (1..N-1).each do |k|
+      break if m[(i+k)%N][j] != m[i][(j+k)%N]
+      v += 1
     end
+    good_m[i][j] = v
   end
-  true
 end
+
+# pp good_m
 
 ans = 0
-N.times do |a|
-  N.times do |b|
-    ans += 1 if good(m, a, b)
+N.times do |i|
+  N.times do |j|
+    good = true
+    N.times do |k|
+      unless good_m[(i+k)%N][(j+k)%N] >= N - 1 - k
+        good = false
+        break
+      end
+    end
+    ans += 1 if good
   end
 end
+
 
 puts ans
