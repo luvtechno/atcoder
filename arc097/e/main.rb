b8037aab0ac41b9e501ddd7cd2481700a7bcd429
Yoshinori Kawasaki <ykawasaki@gmail.com>
2018-05-12 21:52:10 +0900

    Fix

--- arc097/e/main.rb
+++ arc097/e/main.rb
@@ -4,8 +4,8 @@ N = gets.chomp.split(" ").map(&:to_i)
 c = []
 a = []
 (N*2).times do |i|
-  x, y = gets.chomp.split(" ").map(&:to_i)
+  x, y = gets.chomp.split(" ")
   c[i] = x
-  a[i] = y
+  a[i] = y.to_a
 end
 
