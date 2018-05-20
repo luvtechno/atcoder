22ca54b64c8fec34295d9258ced538a86cb88afb
Yoshinori Kawasaki <ykawasaki@gmail.com>
2018-05-20 22:51:58 +0900

    Solve D

--- agc024/d/main.rb
+++ agc024/d/main.rb
@@ -35,31 +35,86 @@ end
 
 
 min_nodes = Set.new
-min = N
+min_depth = N
 N.times do |i|
   s = Set.new
   s << i
 
   d = depth(s, i, edges)
-  if min > d
+  if min_depth > d
     min_nodes = Set.new
     min_nodes << i
-    min = d
-  elsif min == d
+    min_depth = d
+  elsif min_depth == d
     min_nodes << i
   end
   # pp [i, d]
 end
 
-# pp min
+# pp min_depth
 # pp min_nodes.to_a
 
 min_color_count = N
-min_leaf_count = N
+min_leaf_count = 10 ** 10
+
+def child_count(s, i, edges, depth_count_max, depth)
+  s = s.dup
+  s << i
+  nodes = edges[i]
+  nodes = nodes - s
+  size = nodes.size
+  depth_count_max[depth] = size if depth_count_max[depth] < size
+  nodes.each do |j|
+    child_count(s, j, edges, depth_count_max, depth + 1)
+  end
+end
 
 min_nodes.each do |i|
+  depth_count_max = Array.new(min_depth, 1)
+  depth_count_max[0] = 0
+  s = Set.new
+  s << i
+  child_count(s, i, edges, depth_count_max, 0)
+  # pp i
+  # pp edges
+  # pp depth_count_max
+
+  leaf_count = depth_count_max.reduce(:*)
+  color_count = min_depth
+
+  if min_leaf_count > leaf_count
+    min_leaf_count = leaf_count
+    min_color_count = color_count
+  end
+
+  ##
+
+  s = Set.new
+  s << i
+  nodes = edges[i]
+  nodes.each do |j|
+    depth_count_max = Array.new(min_depth, 1)
+    s2 = s.dup
+    s2 << j
+    child_count(s2, i, edges, depth_count_max, 0)
+    child_count(s2, j, edges, depth_count_max, 0)
+    leaf_count = depth_count_max.reduce(:*) * 2
+    d1 = depth(s2, i, edges)
+    d2 = depth(s2, j, edges)
+    color_count = [d1, d2].max
+
+    if min_color_count > color_count
+      min_leaf_count = leaf_count
+      min_color_count = color_count
+    elsif min_color_count == color_count && min_leaf_count > leaf_count
+      min_leaf_count = leaf_count
+      min_color_count = color_count
+    end
+
+  end
+
 
 end
 
 
-puts min_color_count, min_leaf_count
+puts "#{min_color_count} #{min_leaf_count}"
