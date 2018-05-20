13cfc323ff764b8c1716867d017735ba148e59c6
Yoshinori Kawasaki <ykawasaki@gmail.com>
2018-05-20 21:25:26 +0900

    Solve B

--- agc024/b/main.rb
+++ agc024/b/main.rb
@@ -1,6 +1,28 @@
 require 'pp'
 N = gets.chomp.to_i
 p = []
-N.times do
-  p << gets.chomp.to_i
+index = Array.new(N+1)
+N.times do |i|
+  x = gets.chomp.to_i
+  p << x
+  index[x] = i
 end
+
+max = 1
+
+i = 1
+while i <= N
+  current = 1
+  while i < N
+    if index[i] < index[i+1]
+      current += 1
+      max = current if current > max
+      i += 1
+    else
+      break
+    end
+  end
+  i += 1
+end
+
+puts N - max
