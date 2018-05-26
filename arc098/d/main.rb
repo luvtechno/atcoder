6f2b6f26269cddb525d8f6c907059237c5b38bb9
Yoshinori Kawasaki <ykawasaki@gmail.com>
2018-05-26 21:41:56 +0900

    Save

--- arc098/d/main.rb
+++ arc098/d/main.rb
@@ -7,25 +7,27 @@ a = gets.chomp.split(" ").map(&:to_i)
 bits = Array.new(N) { Array.new(20, false) }
 N.times do |i|
   x = a[i]
-  20.times do |j|
+  21.times do |j|
     bits[i][j] = x % 2 == 1
     x /= 2
   end
 end
 
-# pp bits
+pp bits
 
 ans = 0
 
 l = r = 0
+ans_l = 0
 while l < N do
   cur = bits[l].dup
+  puts "l:#{l} r:#{r} ans_l:#{ans_l} ans:#{ans}"
   while r < N do
     if l == r
-      ans += 1
+      ans_l += 1
     else
       flag = false
-      20.times do |j|
+      21.times do |j|
         if bits[r][j]
           if cur[j]
             flag = true
@@ -37,14 +39,26 @@ while l < N do
       if flag
         break
       else
-        ans += 1
+        ans_l += 1
       end
     end
     r += 1
   end
 
+  puts "l:#{l} r:#{r} ans_l:#{ans_l} ans:#{ans}"
+
+  ans += ans_l
+  ans_l -= 1
+
+  21.times do |j|
+    if bits[l][j]
+      if cur[j]
+        cur[j] = false
+      end
+    end
+  end
+
   l += 1
-  r = l
 end
 
 
