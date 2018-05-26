78a47c8a16d9884243c31a56916028134bb2b593
Yoshinori Kawasaki <ykawasaki@gmail.com>
2018-05-26 22:35:41 +0900

    Fail to solve E

--- arc098/e/main.rb
+++ arc098/e/main.rb
@@ -1,4 +1,28 @@
+def binary_search(array, item)
+  first = 0
+  last = array.length - 1
+
+  while first <= last
+    i = (first + last) / 2
+
+    if array[i] == item
+        return i
+    elsif array[i] > item
+        last = i - 1
+    elsif array[i] < item
+        first = i + 1
+    else
+        return nil
+    end
+  end
+end
+
 require 'set'
 require 'pp'
-N, M = gets.chomp.split(" ").map(&:to_i)
-p = gets.chomp.split(" ").map(&:to_i)
+N, K, Q = gets.chomp.split(" ").map(&:to_i)
+a = gets.chomp.split(" ").map(&:to_i)
+
+b = a.sort
+pos = Array.new(N)
+check = Array.new(N, false)
+
