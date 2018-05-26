e16543090cf93729369ac1627f5437aaa7618cae
Yoshinori Kawasaki <ykawasaki@gmail.com>
2018-05-26 22:05:01 +0900

    Save

--- arc098/d/main.rb
+++ arc098/d/main.rb
@@ -18,43 +18,29 @@ end
 ans = 0
 
 l = r = 0
-ans_l = 0
+cur = Array.new(21, 0)
 while l < N do
-  cur = bits[l].dup
-  # puts "l:#{l} r:#{r} ans_l:#{ans_l} ans:#{ans} cur:#{cur}"
+  # puts "l:#{l} r:#{r} ans:#{ans} cur:#{cur}"
   while r < N do
-    if l == r
-      ans_l += 1
-    else
-      flag = false
-      21.times do |j|
-        if bits[r][j] == 1 && cur[j] == 1
-          flag = true
-        end
-      end
-
-      if flag
-        r -= 1
-        break
-      else
-        21.times do |j|
-          if bits[r][j] == 1
-            cur[j] = 1
-          end
-        end
-        ans_l += 1
+    flag = false
+    21.times do |j|
+      if bits[r][j] == 1 && cur[j] == 1
+        flag = true
       end
     end
+    break if flag
+
+    21.times do |j|
+      if bits[r][j] == 1
+        cur[j] = 1
+      end
+    end
+
     r += 1
-    # puts "l:#{l} r:#{r} ans_l:#{ans_l} ans:#{ans} cur:#{cur}"
+    # puts "l:#{l} r:#{r} ans:#{ans} cur:#{cur}"
   end
 
-  # puts "l:#{l} r:#{r} ans_l:#{ans_l} ans:#{ans} cur:#{cur}"
-
-  r -= 1 if r >= N
-  ans += r - l + 1
-  # ans += ans_l
-  # ans_l -= 1
+  ans += r - l
 
   21.times do |j|
     if bits[l][j] == 1
@@ -63,9 +49,6 @@ while l < N do
   end
 
   l += 1
-  if r < l
-    r = l
-  end
 end
 
 
