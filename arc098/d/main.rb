25c2149659d6aa65d7b2892a75f9c928b0c29f81
Yoshinori Kawasaki <ykawasaki@gmail.com>
2018-05-26 22:22:12 +0900

    Optimize

--- arc098/d/main.rb
+++ arc098/d/main.rb
@@ -3,50 +3,24 @@ require 'pp'
 N = gets.chomp.to_i
 a = gets.chomp.split(" ").map(&:to_i)
 
-
-bits = Array.new(N) { Array.new(21, 0) }
-N.times do |i|
-  x = a[i]
-  21.times do |j|
-    bits[i][j] = x % 2
-    x /= 2
-  end
-  # pp [i, bits[i]]
-end
-
-
 ans = 0
 
 l = r = 0
-cur = Array.new(21, 0)
+cur = 0
 while l < N do
   # puts "l:#{l} r:#{r} ans:#{ans} cur:#{cur}"
   while r < N do
-    flag = false
-    21.times do |j|
-      if bits[r][j] == 1 && cur[j] == 1
-        flag = true
-      end
-    end
+    flag = (cur ^ a[r]) != (cur + a[r])
     break if flag
 
-    21.times do |j|
-      if bits[r][j] == 1
-        cur[j] = 1
-      end
-    end
-
+    cur = cur ^ a[r]
     r += 1
     # puts "l:#{l} r:#{r} ans:#{ans} cur:#{cur}"
   end
 
   ans += r - l
 
-  21.times do |j|
-    if bits[l][j] == 1
-      cur[j] = 0
-    end
-  end
+  cur = cur ^ a[l]
 
   l += 1
 end
