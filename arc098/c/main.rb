7b0c93f07d101290b83c7ee8687b5599b411970f
Yoshinori Kawasaki <ykawasaki@gmail.com>
2018-05-26 21:06:13 +0900

    Solve C

--- arc098/c/main.rb
+++ arc098/c/main.rb
@@ -2,3 +2,19 @@ require 'set'
 require 'pp'
 N = gets.chomp.to_i
 s = gets.chomp
+
+e_count = 0
+w_count = 0
+(0..N-1).each do |i|
+  e_count += 1 if s[i] == 'E'
+end
+
+ans = N
+
+N.times do |i|
+  e_count -= 1 if s[i] == 'E'
+  w_count += 1 if i > 0 && s[i-1] == 'W'
+  ans = e_count + w_count if ans > e_count + w_count
+end
+
+puts ans
