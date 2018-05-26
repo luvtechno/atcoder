94c0eeefefa7a1402f34263b5e49e62867cd9f33
Yoshinori Kawasaki <ykawasaki@gmail.com>
2018-05-26 21:20:49 +0900

    Solve D

--- arc098/d/main.rb
+++ arc098/d/main.rb
@@ -2,3 +2,50 @@ require 'set'
 require 'pp'
 N = gets.chomp.to_i
 a = gets.chomp.split(" ").map(&:to_i)
+
+
+bits = Array.new(N) { Array.new(20, false) }
+N.times do |i|
+  x = a[i]
+  20.times do |j|
+    bits[i][j] = x % 2 == 1
+    x /= 2
+  end
+end
+
+# pp bits
+
+ans = 0
+
+l = r = 0
+while l < N do
+  cur = bits[l].dup
+  while r < N do
+    if l == r
+      ans += 1
+    else
+      flag = false
+      20.times do |j|
+        if bits[r][j]
+          if cur[j]
+            flag = true
+          else
+            cur[j] = true
+          end
+        end
+      end
+      if flag
+        break
+      else
+        ans += 1
+      end
+    end
+    r += 1
+  end
+
+  l += 1
+  r = l
+end
+
+
+puts ans
